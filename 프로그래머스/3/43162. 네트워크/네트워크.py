3db1283b3c4c6b6756159a394281b3f226627603
Subin Oh <63906691+eourm20@users.noreply.github.com>
2025-05-30 13:55:07 +0900

    [level 3] Title: 네트워크, Time: 0.51 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,15 @@
+def solution(n, computers):
+    visited = [False] * n
+
+    def dfs(cur):
+        visited[cur] = True
+        for next in range(n):
+            if computers[cur][next] == 1 and not visited[next]:
+                dfs(next)
+
+    answer = 0
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+    return answer
