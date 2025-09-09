107c1292b5dee9fbba8e0b0aee81ee9931be8463
Subin Oh <63906691+eourm20@users.noreply.github.com>
2025-09-09 22:43:32 +0900

    [Gold V] Title: 회의실 배정, Time: 260 ms, Memory: 54216 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -3,9 +3,9 @@ input = sys.stdin.readline
 
 n = int(input())
 info = [tuple(map(int, input().split())) for _ in range(n)]
-info = [(x[0], x[1], x[1]-x[0]) for x in info]
 info.sort(key=lambda x: (x[0], x[1]), reverse=(True, False))
-
+# ❌ Actual Output:
+# [(12, 14, 2), (8, 11, 3), (8, 12, 4), (6, 10, 4), (5, 7, 2), (5, 9, 4), (3, 5, 2), (3, 8, 5), (2, 13, 11), (1, 4, 3), (0, 6, 6)]
 answer = 1
 s = 0
 for i in range(s+1, n):
@@ -13,4 +13,5 @@ for i in range(s+1, n):
         answer += 1
         s = i
 
+    
 print(answer)
