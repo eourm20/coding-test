905bd4965424384523d1e5a882e45f255bbb0fb2
Subin Oh <63906691+eourm20@users.noreply.github.com>
2025-10-10 22:56:24 +0900

    [Silver I] Title: 안정적인 문자열, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+number = 1
+while True:
+    s = input().rstrip()
+    if '-' in s:
+        break
+    
+    ss = []
+    change = 0
+    
+    for ch in range(len(s)):
+        if ch == 0:
+            if s[ch] == '}':
+                change += 1
+                ss.append('{')
+            else:
+                ss.append('{')
+        else:
+            if s[ch] == '{':
+                ss.append('{')
+            elif s[ch] == '}':
+                if len(ss) != 0:
+                    ss.pop()
+                else:
+                    change += 1
+                    ss.append('{')
+                    
+    # print(ss)
+    change += len(ss)//2
+    print(f"{number}. {change}")
+    number += 1
